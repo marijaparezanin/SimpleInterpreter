3ec32f233f72e5a830598097374cfea20da9ec66
marijaparezanin <marija.parezanin@mensa.ba>
2024-12-29 15:36:35 +0100

    [MOD] - Add task example and have interpreter work with files instead of strings

--- SimpleInterpreter/src/Main.java
+++ SimpleInterpreter/src/Main.java
@@ -3,27 +3,9 @@ import runtime.*;
 
 public class Main {
     public static void main(String[] args) {
-        String code = """
-x = 1
-print x
-scope {
- x = 2
- print x
- scope {
-   x = 3
-   y = x
-   print x
-   print y
- }
- print x
- print y
-}
-print x
-        """;
-
-
+        String taskExamplePath = "src/taskExample.txt";
         Interpreter interpreter = new Interpreter();
-        interpreter.interpret(code);
+        interpreter.interpret(taskExamplePath);
 
 
     }
--- SimpleInterpreter/src/runtime/Interpreter.java
+++ SimpleInterpreter/src/runtime/Interpreter.java
@@ -4,7 +4,9 @@ import parser.Parser;
 import parser.BlockNode;
 import lexer.Lexer;
 import lexer.Token;
+import utils.FileReader;
 
+import java.io.IOException;
 import java.util.List;
 
 public class Interpreter {
@@ -14,7 +16,13 @@ public class Interpreter {
         this.scopeManager = new ScopeManager();
     }
 
-    public void interpret(String code) {
+    public void interpret(String filePath) {
+        String code = null;
+        try {
+            code = FileReader.readProgram(filePath);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         Lexer lexer = new Lexer();
         List<Token> tokens = lexer.tokenize(code);
         Parser parser = new Parser(tokens);
